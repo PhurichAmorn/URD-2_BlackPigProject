6af4169cd02499ddfc21a49091c904cac166d70f
PhurichAmorn <phurich.amorn@gmail.com>
2026-03-28 09:51:58 +0700

    format dart

--- DooMoo/test/pixel_to_metric_test.dart
+++ DooMoo/test/pixel_to_metric_test.dart
@@ -26,202 +26,202 @@ void main() {
       expect(resultMm, closeTo(381.679, 0.001));
     });
 
-      test('when pixel length changes then result scales proportionally', () {
-    final focalLength = 5.24;
-    final sensorWidth = 7.68;
-    final sensorHeight = 5.76;
-    final imageWidth = 3000;
-    final imageHeight = 4000;
-    final distanceMm = 1000.0;
-    final pixelLength = 500.0;
+    test('when pixel length changes then result scales proportionally', () {
+      final focalLength = 5.24;
+      final sensorWidth = 7.68;
+      final sensorHeight = 5.76;
+      final imageWidth = 3000;
+      final imageHeight = 4000;
+      final distanceMm = 1000.0;
+      final pixelLength = 500.0;
 
-    final resultMm = PigMath.pixelToMm(
-      pixelLength: pixelLength,
-      distanceMm: distanceMm,
-      focalLength: focalLength,
-      sensorWidth: sensorWidth,
-      sensorHeight: sensorHeight,
-      imageWidth: imageWidth,
-      imageHeight: imageHeight,
-    );
+      final resultMm = PigMath.pixelToMm(
+        pixelLength: pixelLength,
+        distanceMm: distanceMm,
+        focalLength: focalLength,
+        sensorWidth: sensorWidth,
+        sensorHeight: sensorHeight,
+        imageWidth: imageWidth,
+        imageHeight: imageHeight,
+      );
 
-    expect(resultMm, closeTo(190.8395, 0.001));
-  });
+      expect(resultMm, closeTo(190.8395, 0.001));
+    });
 
     test('when pixel length is zero then returns zero', () {
-    final focalLength = 5.24;
-    final sensorWidth = 7.68;
-    final sensorHeight = 5.76;
-    final imageWidth = 3000;
-    final imageHeight = 4000;
-    final distanceMm = 1000.0;
-    final pixelLength = 0.0;
+      final focalLength = 5.24;
+      final sensorWidth = 7.68;
+      final sensorHeight = 5.76;
+      final imageWidth = 3000;
+      final imageHeight = 4000;
+      final distanceMm = 1000.0;
+      final pixelLength = 0.0;
 
-    final resultMm = PigMath.pixelToMm(
-      pixelLength: pixelLength,
-      distanceMm: distanceMm,
-      focalLength: focalLength,
-      sensorWidth: sensorWidth,
-      sensorHeight: sensorHeight,
-      imageWidth: imageWidth,
-      imageHeight: imageHeight,
-    );
+      final resultMm = PigMath.pixelToMm(
+        pixelLength: pixelLength,
+        distanceMm: distanceMm,
+        focalLength: focalLength,
+        sensorWidth: sensorWidth,
+        sensorHeight: sensorHeight,
+        imageWidth: imageWidth,
+        imageHeight: imageHeight,
+      );
 
-    expect(resultMm, 0);
-  });
+      expect(resultMm, 0);
+    });
 
     test('when distance increases then real world size increases', () {
-    final focalLength = 5.24;
-    final sensorWidth = 7.68;
-    final sensorHeight = 5.76;
-    final imageWidth = 3000;
-    final imageHeight = 4000;
-    final distanceMm = 2000.0;
-    final pixelLength = 1000.0;
+      final focalLength = 5.24;
+      final sensorWidth = 7.68;
+      final sensorHeight = 5.76;
+      final imageWidth = 3000;
+      final imageHeight = 4000;
+      final distanceMm = 2000.0;
+      final pixelLength = 1000.0;
 
-    final resultMm = PigMath.pixelToMm(
-      pixelLength: pixelLength,
-      distanceMm: distanceMm,
-      focalLength: focalLength,
-      sensorWidth: sensorWidth,
-      sensorHeight: sensorHeight,
-      imageWidth: imageWidth,
-      imageHeight: imageHeight,
-    );
+      final resultMm = PigMath.pixelToMm(
+        pixelLength: pixelLength,
+        distanceMm: distanceMm,
+        focalLength: focalLength,
+        sensorWidth: sensorWidth,
+        sensorHeight: sensorHeight,
+        imageWidth: imageWidth,
+        imageHeight: imageHeight,
+      );
 
-    expect(resultMm, greaterThan(381.679));
-  });
+      expect(resultMm, greaterThan(381.679));
+    });
 
-  test('when pixel length is very small then still computes value', () {
-    final focalLength = 5.24;
-    final sensorWidth = 7.68;
-    final sensorHeight = 5.76;
-    final imageWidth = 3000;
-    final imageHeight = 4000;
-    final distanceMm = 1000.0;
-    final pixelLength = 0.0001;
+    test('when pixel length is very small then still computes value', () {
+      final focalLength = 5.24;
+      final sensorWidth = 7.68;
+      final sensorHeight = 5.76;
+      final imageWidth = 3000;
+      final imageHeight = 4000;
+      final distanceMm = 1000.0;
+      final pixelLength = 0.0001;
 
-    final resultMm = PigMath.pixelToMm(
-      pixelLength: pixelLength,
-      distanceMm: distanceMm,
-      focalLength: focalLength,
-      sensorWidth: sensorWidth,
-      sensorHeight: sensorHeight,
-      imageWidth: imageWidth,
-      imageHeight: imageHeight,
-    );
+      final resultMm = PigMath.pixelToMm(
+        pixelLength: pixelLength,
+        distanceMm: distanceMm,
+        focalLength: focalLength,
+        sensorWidth: sensorWidth,
+        sensorHeight: sensorHeight,
+        imageWidth: imageWidth,
+        imageHeight: imageHeight,
+      );
 
-    expect(resultMm, greaterThan(0));
-  });
+      expect(resultMm, greaterThan(0));
+    });
 
-  test('when pixel length is extremely large then still returns value', () {
-    final focalLength = 5.24;
-    final sensorWidth = 7.68;
-    final sensorHeight = 5.76;
-    final imageWidth = 3000;
-    final imageHeight = 4000;
-    final distanceMm = 1000.0;
-    final pixelLength = 1000000.0;
+    test('when pixel length is extremely large then still returns value', () {
+      final focalLength = 5.24;
+      final sensorWidth = 7.68;
+      final sensorHeight = 5.76;
+      final imageWidth = 3000;
+      final imageHeight = 4000;
+      final distanceMm = 1000.0;
+      final pixelLength = 1000000.0;
 
-    final resultMm = PigMath.pixelToMm(
-      pixelLength: pixelLength,
-      distanceMm: distanceMm,
-      focalLength: focalLength,
-      sensorWidth: sensorWidth,
-      sensorHeight: sensorHeight,
-      imageWidth: imageWidth,
-      imageHeight: imageHeight,
-    );
+      final resultMm = PigMath.pixelToMm(
+        pixelLength: pixelLength,
+        distanceMm: distanceMm,
+        focalLength: focalLength,
+        sensorWidth: sensorWidth,
+        sensorHeight: sensorHeight,
+        imageWidth: imageWidth,
+        imageHeight: imageHeight,
+      );
 
-    expect(resultMm, isNotNull);
-  });
+      expect(resultMm, isNotNull);
+    });
 
     test('when focal length is zero then returns null', () {
-    final focalLength = 0.0;
-    final sensorWidth = 7.68;
-    final sensorHeight = 5.76;
-    final imageWidth = 3000;
-    final imageHeight = 4000;
-    final distanceMm = 1000.0;
-    final pixelLength = 1000.0;
+      final focalLength = 0.0;
+      final sensorWidth = 7.68;
+      final sensorHeight = 5.76;
+      final imageWidth = 3000;
+      final imageHeight = 4000;
+      final distanceMm = 1000.0;
+      final pixelLength = 1000.0;
 
-    final resultMm = PigMath.pixelToMm(
-      pixelLength: pixelLength,
-      distanceMm: distanceMm,
-      focalLength: focalLength,
-      sensorWidth: sensorWidth,
-      sensorHeight: sensorHeight,
-      imageWidth: imageWidth,
-      imageHeight: imageHeight,
-    );
+      final resultMm = PigMath.pixelToMm(
+        pixelLength: pixelLength,
+        distanceMm: distanceMm,
+        focalLength: focalLength,
+        sensorWidth: sensorWidth,
+        sensorHeight: sensorHeight,
+        imageWidth: imageWidth,
+        imageHeight: imageHeight,
+      );
 
-    expect(resultMm, isNull);
-  });
+      expect(resultMm, isNull);
+    });
 
-  test('when image width is zero then returns null', () {
-    final focalLength = 5.24;
-    final sensorWidth = 7.68;
-    final sensorHeight = 5.76;
-    final imageWidth = 0;
-    final imageHeight = 4000;
-    final distanceMm = 1000.0;
-    final pixelLength = 1000.0;
+    test('when image width is zero then returns null', () {
+      final focalLength = 5.24;
+      final sensorWidth = 7.68;
+      final sensorHeight = 5.76;
+      final imageWidth = 0;
+      final imageHeight = 4000;
+      final distanceMm = 1000.0;
+      final pixelLength = 1000.0;
 
-    final resultMm = PigMath.pixelToMm(
-      pixelLength: pixelLength,
-      distanceMm: distanceMm,
-      focalLength: focalLength,
-      sensorWidth: sensorWidth,
-      sensorHeight: sensorHeight,
-      imageWidth: imageWidth,
-      imageHeight: imageHeight,
-    );
+      final resultMm = PigMath.pixelToMm(
+        pixelLength: pixelLength,
+        distanceMm: distanceMm,
+        focalLength: focalLength,
+        sensorWidth: sensorWidth,
+        sensorHeight: sensorHeight,
+        imageWidth: imageWidth,
+        imageHeight: imageHeight,
+      );
 
-    expect(resultMm, isNull);
-  });
+      expect(resultMm, isNull);
+    });
 
-  test('when sensor width is zero then returns null', () {
-    final focalLength = 5.24;
-    final sensorWidth = 0.0;
-    final sensorHeight = 5.76;
-    final imageWidth = 3000;
-    final imageHeight = 4000;
-    final distanceMm = 1000.0;
-    final pixelLength = 1000.0;
+    test('when sensor width is zero then returns null', () {
+      final focalLength = 5.24;
+      final sensorWidth = 0.0;
+      final sensorHeight = 5.76;
+      final imageWidth = 3000;
+      final imageHeight = 4000;
+      final distanceMm = 1000.0;
+      final pixelLength = 1000.0;
 
-    final resultMm = PigMath.pixelToMm(
-      pixelLength: pixelLength,
-      distanceMm: distanceMm,
-      focalLength: focalLength,
-      sensorWidth: sensorWidth,
-      sensorHeight: sensorHeight,
-      imageWidth: imageWidth,
-      imageHeight: imageHeight,
-    );
+      final resultMm = PigMath.pixelToMm(
+        pixelLength: pixelLength,
+        distanceMm: distanceMm,
+        focalLength: focalLength,
+        sensorWidth: sensorWidth,
+        sensorHeight: sensorHeight,
+        imageWidth: imageWidth,
+        imageHeight: imageHeight,
+      );
 
-    expect(resultMm, isNull);
-  });
+      expect(resultMm, isNull);
+    });
     test('when sensor height is zero then returns null', () {
-    final focalLength = 5.24;
-    final sensorWidth = 7.68;
-    final sensorHeight = 0.0;
-    final imageWidth = 3000;
-    final imageHeight = 4000;
-    final distanceMm = 1000.0;
-    final pixelLength = 1000.0;
+      final focalLength = 5.24;
+      final sensorWidth = 7.68;
+      final sensorHeight = 0.0;
+      final imageWidth = 3000;
+      final imageHeight = 4000;
+      final distanceMm = 1000.0;
+      final pixelLength = 1000.0;
 
-    final resultMm = PigMath.pixelToMm(
-      pixelLength: pixelLength,
-      distanceMm: distanceMm,
-      focalLength: focalLength,
-      sensorWidth: sensorWidth,
-      sensorHeight: sensorHeight,
-      imageWidth: imageWidth,
-      imageHeight: imageHeight,
-    );
+      final resultMm = PigMath.pixelToMm(
+        pixelLength: pixelLength,
+        distanceMm: distanceMm,
+        focalLength: focalLength,
+        sensorWidth: sensorWidth,
+        sensorHeight: sensorHeight,
+        imageWidth: imageWidth,
+        imageHeight: imageHeight,
+      );
 
-    expect(resultMm, isNull);
-  });
+      expect(resultMm, isNull);
+    });
 
     test('when distance is zero then returns null', () {
       final focalLength = 5.24;
@@ -268,47 +268,47 @@ void main() {
     });
 
     test('when focal length is negative then returns null', () {
-    final focalLength = -5.24;
-    final sensorWidth = 7.68;
-    final sensorHeight = 5.76;
-    final imageWidth = 3000;
-    final imageHeight = 4000;
-    final distanceMm = 1000.0;
-    final pixelLength = 1000.0;
+      final focalLength = -5.24;
+      final sensorWidth = 7.68;
+      final sensorHeight = 5.76;
+      final imageWidth = 3000;
+      final imageHeight = 4000;
+      final distanceMm = 1000.0;
+      final pixelLength = 1000.0;
 
-    final resultMm = PigMath.pixelToMm(
-      pixelLength: pixelLength,
-      distanceMm: distanceMm,
-      focalLength: focalLength,
-      sensorWidth: sensorWidth,
-      sensorHeight: sensorHeight,
-      imageWidth: imageWidth,
-      imageHeight: imageHeight,
-    );
+      final resultMm = PigMath.pixelToMm(
+        pixelLength: pixelLength,
+        distanceMm: distanceMm,
+        focalLength: focalLength,
+        sensorWidth: sensorWidth,
+        sensorHeight: sensorHeight,
+        imageWidth: imageWidth,
+        imageHeight: imageHeight,
+      );
 
-    expect(resultMm, isNull);
-  });
+      expect(resultMm, isNull);
+    });
 
     test('when multiple parameters are invalid then returns null', () {
-    final focalLength = 0.0;
-    final sensorWidth = 0.0;
-    final sensorHeight = 5.76;
-    final imageWidth = 0;
-    final imageHeight = 4000;
-    final distanceMm = -100.0;
-    final pixelLength = -50.0;
+      final focalLength = 0.0;
+      final sensorWidth = 0.0;
+      final sensorHeight = 5.76;
+      final imageWidth = 0;
+      final imageHeight = 4000;
+      final distanceMm = -100.0;
+      final pixelLength = -50.0;
 
-    final resultMm = PigMath.pixelToMm(
-      pixelLength: pixelLength,
-      distanceMm: distanceMm,
-      focalLength: focalLength,
-      sensorWidth: sensorWidth,
-      sensorHeight: sensorHeight,
-      imageWidth: imageWidth,
-      imageHeight: imageHeight,
-    );
+      final resultMm = PigMath.pixelToMm(
+        pixelLength: pixelLength,
+        distanceMm: distanceMm,
+        focalLength: focalLength,
+        sensorWidth: sensorWidth,
+        sensorHeight: sensorHeight,
+        imageWidth: imageWidth,
+        imageHeight: imageHeight,
+      );
 
-    expect(resultMm, isNull);
-  });
+      expect(resultMm, isNull);
+    });
   });
 }
